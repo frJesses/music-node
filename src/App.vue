ded7127bab2d62777f1fee75d49be30419e6f864
frank <1547531839@qq.com>
2020-12-02 14:25:12 +0800

    fir

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{count}}</p>
+    <button @click="myFn">按钮</button>
+  </div>
+</template>
+
+<script>
+  import { shallowRef } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    // 监听数据变化
+    let count = shallowRef(10)
+    function myFn() {
+
+    }
+  }
+}
+</script>
